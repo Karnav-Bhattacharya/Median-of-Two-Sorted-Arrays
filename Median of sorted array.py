72dacee3009ae738ae850b760e5477b232804512
Karnav-Bhattacharya <85335851+Karnav-Bhattacharya@users.noreply.github.com>
2021-07-13 13:57:38 +0530

    Add files via upload

---
+++ Median of sorted array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def median(self, nums1, nums2) -> float:
+        lst = nums1 + nums2
+        lst.sort()
+        n = len(lst)
+        if n%2 == 0:
+            return float((lst[int(n/2)-1] + lst[int((n/2)+1)-1])/2)
+
+        else:
+            return float(lst[int(n/2)])
+
+answer = Solution()
+print(answer.median(nums1=[1, 3], nums2=[2]))
+# print()
